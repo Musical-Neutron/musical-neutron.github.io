5df1a3122116f5c4b08a4f4345ad690752859ff1
Oliver Newton <oliver.newton.92@gmail.com>
2020-05-18 00:46:24 +0200

    Adding footer.js, word tweaks on index.

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.write(`
+    <div class="inner">
+        <ul class="menu">
+            <li>&copy; Oliver Newton 2018&ndash;2020. All rights reserved.</li>
+            <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+        </ul>
+    </div>
+`);
