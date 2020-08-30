f2088039bc4a58390be44b228a320108ba66365d
Oliver Newton <oliver.newton.92@gmail.com>
2020-08-30 15:14:59 +0200

    Converting headers/footers to js files

---
+++ header.js
@@ -0,0 +1,24 @@
+document.write(`
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="description"
+        content="Postdoctoral researcher interested in the Milky Way's satellite galaxies and the nature of dark matter." />
+    <meta name="copyright" content="&copy; Oliver Newton 2018&ndash;2019" />
+    <meta name="robots" content="index,follow" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128074602-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-128074602-2');
+    </script>
+`);
