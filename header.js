a42d9e05d12488545276a649523c91de87e2e2f8
ONewton <olivier.newton@univ-lyon1.fr>
2022-05-18 00:36:41 +0100

    Fixing typo in header script

--- header.js
+++ header.js
@@ -18,8 +18,7 @@ document.write(`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-        
           gtag('config', 'G-KNXLD374BC');
         </script>
-    <script>
+    </script>
 `);
